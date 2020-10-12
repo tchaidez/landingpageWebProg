45e60906ab9f757d880b4e5b7b888e6828ea47f2
tchaidez <71134934+tchaidez@users.noreply.github.com>
2020-10-12 05:21:16 -0500

    Add files via upload

---
+++ landingPageScript.js
@@ -0,0 +1,130 @@
+
+/*
+
+template I changed for hand & show:
+
+https://www.w3schools.com/howto/howto_js_toggle_hide_show.asp
+
+*/
+
+function hideTextSprint1() {
+
+    var text = document.getElementById("Sprint-1-Items");
+    
+    if(text.style.display == "none"){
+
+        text.style.display = "block";
+    }
+
+    else if(text.style.display != "none"){
+
+        text.style.display = "none";
+    }
+}
+
+    function hideTextSprint2() {
+
+    var text = document.getElementById("Sprint-2-Items");
+
+    if(text.style.display == "none"){
+
+        text.style.display = "block";
+    }
+
+    else if(text.style.display != "none"){
+
+        text.style.display = "none";
+    }
+
+
+}
+
+function hideTextSprint3() {
+
+    var text = document.getElementById("Sprint-3-Items");
+    
+    if(text.style.display == "none"){
+
+        text.style.display = "block";
+    }
+
+    else if(text.style.display != "none"){
+
+        text.style.display = "none";
+    }
+}
+
+function hideTextSprint4() {
+
+    var text = document.getElementById("Sprint-4-Items");
+    
+    if(text.style.display == "none"){
+
+        text.style.display = "block";
+    }
+
+    else if(text.style.display != "none"){
+
+        text.style.display = "none";
+    }
+}
+
+function hideTextSprint5() {
+
+    var text = document.getElementById("Sprint-5-Items");
+    
+    if(text.style.display == "none"){
+
+        text.style.display = "block";
+    }
+
+    else if(text.style.display != "none"){
+
+        text.style.display = "none";
+    }
+}
+
+function hideTextSprint6() {
+
+    var text = document.getElementById("Sprint-6-Items");
+    
+    if(text.style.display == "none"){
+
+        text.style.display = "block";
+    }
+
+    else if(text.style.display != "none"){
+
+        text.style.display = "none";
+    }
+}
+
+function hideTextSprint7() {
+
+    var text = document.getElementById("Sprint-7-Items");
+    
+    if(text.style.display == "none"){
+
+        text.style.display = "block";
+    }
+
+    else if(text.style.display != "none"){
+
+        text.style.display = "none";
+    }
+}
+
+function hideTextSprint8() {
+
+    var text = document.getElementById("Sprint-8-Items");
+    
+    if(text.style.display == "none"){
+
+        text.style.display = "block";
+    }
+
+    else if(text.style.display != "none"){
+
+        text.style.display = "none";
+    }
+}
